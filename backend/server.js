ed761e072d6a64dffae1959fd73ddf3df39418e4
[DAGOATTTT] <104139331+DAGOATTTT@users.noreply.github.com>
2023-02-28 20:09:31 -0600

    Integrated signup and added cors

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,14 @@ const connectDB = require('./config/db')
 
 connectDB()
 const app = express()
+const cors = require('cors')
 
+
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+
 app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
