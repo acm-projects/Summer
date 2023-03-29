185455c071cc9e475f8f215991106c9df41770df
Dan Dan <dh.davila@hotmail.com>
2023-03-29 17:24:12 -0500

    quiz controller created

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -9,19 +9,32 @@ API ENDPOINT: https://api.openai.com/v1/completions
 const createQuiz = asyncHandler( async(req, res) => {
   // const {text} = req.body;
 
-const configuration = new Configuration({
-  apiKey: process.env.OpenAI_SECRET,
-});
-const openai = new OpenAIApi(configuration);
-const response = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "Consisting of 2 multiple-choice questions with 4 answer choices each, generate a quiz based off of the following text: " + transcript,
-  max_tokens: 7,
-  temperature: 0,
-});
+  const configuration = new Configuration({
+    apiKey: process.env.OpenAI_SECRET,
+  });
 
+  const openai = new OpenAIApi(configuration);
 
-})
+  try {
+    const openai = new OpenAIApi(configuration);
+
+    openai.createCompletion ({
+    model: "text-davinci-003",
+    prompt: "Generate a short quiz of 3 questions with 4 answer choices about the following text: " + transcript,
+    max_tokens: 150,
+    temperature: 0,
+    })
+    .then((response) => {
+      //res.sendStatus(response.data.choices)
+      res.json({summary: response.data.choices[0].text});
+    })
+
+  } catch(error) {
+    res.status(500)
+    throw new Error('Failed quiz generation');
+  }
+
+});
 
 module.exports = {
   createQuiz 
