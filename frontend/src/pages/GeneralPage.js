46d0f613cd82cb733e74c77e70b518d93426ac3c
matth101 <matt10145h@gmail.com>
2023-04-13 17:01:25 -0500

    MANUALLY resolve all conflicts; correct frontend

--- frontend/src/pages/GeneralPage.js
+++ frontend/src/pages/GeneralPage.js
@@ -13,6 +13,11 @@ const GeneralPage = () => {
 		navigate('/summary')
 	}
 
+	function handleQuiz(e) {
+		e.preventDefault()
+		navigate('/quizpage')
+	}
+
 	return (
 		<div className={styles.pageContainer}>
 			<div className={styles.videoSection}>
@@ -48,7 +53,7 @@ const GeneralPage = () => {
 						<div className={`${styles.viewVidBtn} ${styles.summaryBtn}`}>VIEW SUMMARY</div>
 					</div>
 					<div className={styles.quiz}>
-						<div className={`${styles.viewVidBtn} ${styles.quizBtn}`}>
+						<div className={`${styles.viewVidBtn} ${styles.quizBtn}`} onClick={handleQuiz}>
 							TAKE QUIZ
 						</div>
 					</div>
