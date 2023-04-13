7392250393680c2611c5d15c19b03f78e8cd5d7d
Daniel <dh.davila@hotmail.com>
2023-04-12 22:00:56 -0600

    added to quizroutes

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {createQuiz} = require('../controllers/quizController')
+const {createQuiz, getQuiz} = require('../controllers/quizController')
 
 
 router.post('/', createQuiz)
